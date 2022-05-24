84cdf1e422030ca03cb8bd70a204551f3cb958c2
MukhtarOsmani <mukhtar.a.osmani@gmail.com>
2022-05-23 21:36:37 -0400

    bootstrap project first commit

---
+++ js/myjs.js
@@ -0,0 +1,22 @@
+function add()
+{
+    var fn = 22;
+    var sn = 33;
+    var result = fn + sn;
+
+    document.getElementById("showAddResult").innerHTML = result;
+}
+
+
+function showCompanyName()
+{
+
+    var CompnayName = "Arian company";
+
+
+    document.getElementById("one").innerHTML = CompnayName 
+}
+
+
+
+
